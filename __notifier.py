46246b2b82abfbc86ce03b67d75cf01a355789ce
xushier <31540107+xushier@users.noreply.github.com>
2022-02-28 18:03:12 +0800

    Update __notifier.py

--- __notifier.py
+++ __notifier.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 # Created By Xushier  QQ:1575659493
 
-from __logger import Logger
 import requests
 
 class SendNotify(object):
@@ -9,18 +8,17 @@ class SendNotify(object):
     def __init__(self, token:str) -> None:
         self.token   = token
         self.url     = 'http://www.pushplus.plus/send?' + 'token=' + self.token
-        self.log     = Logger(file_name='run.log', level='info', when='D', backCount=5, interval=1)
 
     def pushplus(self, title:str, content:str) -> None:
         if self.token:
             pushplus_headers = {'Content-Type':'application/json'}
             pushplus_req = requests.get(self.url+'&title='+title+'&content='+content,headers=pushplus_headers)
             if pushplus_req.status_code == 200:
-                self.log.info("通知发送成功！")
+                print("通知发送成功！")
             else:
-                self.log.warning("通知发送失败！")
+                print("通知发送失败！")
         else:
-            self.log.warning("pushplus token 未设置！")
+            print("pushplus token 未设置！")
 
 
 

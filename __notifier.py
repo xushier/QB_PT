0837f14580627202f3eb0d79e285519e1de84b91
xushier <31540107+xushier@users.noreply.github.com>
2022-02-28 13:28:15 +0800

    Update __notifier.py

--- __notifier.py
+++ __notifier.py
@@ -12,12 +12,15 @@ class SendNotify(object):
         self.log     = Logger(file_name='run.log', level='info', when='D', backCount=5, interval=1)
 
     def pushplus(self, title:str, content:str) -> None:
-        pushplus_headers = {'Content-Type':'application/json'}
-        pushplus_req = requests.get(self.url+'&title='+title+'&content='+content,headers=pushplus_headers)
-        if pushplus_req.status_code == 200:
-            self.log.info("通知发送成功！")
-        else:
-            self.log.warning("通知发送失败！")
+        if self.token:
+            pushplus_headers = {'Content-Type':'application/json'}
+            pushplus_req = requests.get(self.url+'&title='+title+'&content='+content,headers=pushplus_headers)
+            if pushplus_req.status_code == 200:
+                self.log.info("通知发送成功！")
+            else:
+                self.log.warning("通知发送失败！")
+        else：
+            self.log.warning("pushplus token 未设置！")
 
 
 

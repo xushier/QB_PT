3a49d905dd279049f5ccc5d68042ca04c8a3c553
xushier <31540107+xushier@users.noreply.github.com>
2022-03-01 15:59:20 +0800

    Update reannounce.py

--- reannounce.py
+++ reannounce.py
@@ -10,10 +10,9 @@ new Env('重新汇报前15个种子');
 
 from __qbittorrent import Client
 
-if __name__ == '__main__':
-    qb        = Client()
-    torrents  = qb.filter_torrents(filter='all', limit=15, sorted='added_on', reverse='True')
-    info_hash = []
-    for torrent in torrents:
-        info_hash.append(torrent['hash'])
-    qb.reannounce(info_hash)
+qb        = Client()
+torrents  = qb.filter_torrents(filter='all', limit=15, sorted='added_on', reverse='True')
+info_hash = []
+for torrent in torrents:
+    info_hash.append(torrent['hash'])
+qb.reannounce(info_hash)

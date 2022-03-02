7ab681e504dd37542f027fa8664a36fe760b5b14
xushier <31540107+xushier@users.noreply.github.com>
2022-03-02 20:30:09 +0800

    Update __notifier.py

--- __notifier.py
+++ __notifier.py
@@ -32,7 +32,7 @@ push_config = {
 
     'PUSH_KEY': '',                     # server 酱的 PUSH_KEY，兼容旧版与 Turbo 版
 
-    'PUSH_PLUS_TOKEN': '',        # push+ 微信推送的用户令牌
+    'PUSH_PLUS_TOKEN': '',              # push+ 微信推送的用户令牌
     'PUSH_PLUS_USER': '',               # push+ 微信推送的群组编码
 
     'QMSG_KEY': '',                     # qmsg 酱的 QMSG_KEY
